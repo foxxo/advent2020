1c76958fad21499e0104ce3696b5cb716658ea10
Toddo <foxxo.net@gmail.com>
2020-12-04 11:59:03 -0500

    Added AdventUtil class to handle the basic file reading stage for each day

---
+++ src/main/java/AdventUtil/AdventUtil.java
@@ -0,0 +1,22 @@
+package AdventUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class AdventUtil {
+
+    public static List<String> readInputLines(String fileName) throws IOException {
+        Path path = Paths.get("src/main/resources/" + fileName + " txt");
+
+
+        BufferedReader reader = Files.newBufferedReader(path);
+        Stream<String> lines = reader.lines();
+        return lines.collect(Collectors.toList());
+    }
+}
--- src/main/java/DayFour/DayFour.java
+++ src/main/java/DayFour/DayFour.java
@@ -1,24 +1,15 @@
 package DayFour;
 
-import java.io.BufferedReader;
+import AdventUtil.AdventUtil;
+
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class DayFour {
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("src/main/resources/input4.txt");
-
-
-        BufferedReader reader = Files.newBufferedReader(path);
-        Stream<String> lines = reader.lines();
-        List<String> inputLines = lines.collect(Collectors.toList());
+        List<String> inputLines = AdventUtil.readInputLines("input4");
 
         List<Passport> passports = extractPassports(inputLines);
 
