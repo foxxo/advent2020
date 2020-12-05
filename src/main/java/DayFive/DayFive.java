d4c8dee4f2e2d393dfa202d45fe3ff6009fc049b
Tod Schlegelmilch <tschlegelmilch@pillartechnology.com>
2020-12-05 10:01:33 -0500

    Day 5 part 2 complete

--- src/main/java/DayFive/DayFive.java
+++ src/main/java/DayFive/DayFive.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import java.util.List;
 
 public class DayFive {
-    static final int ROWS = 7;
+    static final int ROWS = 127;
+    static final int COLS = 7;
+    static boolean[][] seatsTaken = new boolean[ROWS+1][COLS+1];
 
     public static void main(String[] args) throws IOException {
+
         List<String> inputLines = AdventUtil.readInputLines("input5");
 
         int max = 0;
+
 
         for(String s : inputLines)
         {
@@ -20,6 +24,14 @@ public class DayFive {
         }
 
         System.out.println("Highest seat number: " + max);
+
+        for(int x = 1; x < ROWS-1; x++)
+            for(int y = 1; y < COLS-1; y++)
+            {
+                if(!seatsTaken[x][y])
+                    if(seatsTaken[x][y-1] && seatsTaken[x][y+1])
+                    System.out.println("Found my seat! Row " + x + ", Col " + y + ", Seat #" + (8 * x + y));
+            }
     }
 
     static int evaluateTicket(String input)
@@ -27,11 +39,11 @@ public class DayFive {
         int row = -1;
         int col = -1;
 
-        row = binaryEval(input.substring(0, ROWS), 0, 127, 'F', 'B');
-        col = binaryEval(input.substring(ROWS), 0, 7, 'L', 'R');
+        row = binaryEval(input.substring(0, COLS), 0, ROWS, 'F', 'B');
+        col = binaryEval(input.substring(COLS), 0, COLS, 'L', 'R');
 
         int seatNum = 8 * row + col;
-        System.out.println(String.format("Seat #%d, row %d, column %d", seatNum, row, col));
+        seatsTaken[row][col] = true;
         return seatNum;
     }
 
@@ -43,13 +55,11 @@ public class DayFive {
         }
         if(input.charAt(0) == firstHalfChar)
         {
-            max -= diff / 2;
-            max--;
+            max -= diff / 2 +1;
         }
         else if(input.charAt(0) == lastHalfChar)
         {
-            min += diff / 2;
-            min++;
+            min += diff / 2 +1;
         }
         return binaryEval(input.substring(1), min, max, firstHalfChar, lastHalfChar);
     }
