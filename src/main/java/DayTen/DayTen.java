fcdc63a62822407ea9430e1c93b22d47e1b4b0a1
Toddo <foxxo.net@gmail.com>
2020-12-10 14:05:32 -0500

    Day 10 part 2. Had to cheat for the math!

--- src/main/java/DayTen/DayTen.java
+++ src/main/java/DayTen/DayTen.java
@@ -3,9 +3,7 @@ package DayTen;
 import AdventUtil.AdventUtil;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DayTen {
@@ -22,12 +20,21 @@ public class DayTen {
 
         System.out.println("Joltage diffs: " + joltageDifferences);
         System.out.println("1's x 3's: " + joltageDifferences.get(1) * joltageDifferences.get(3));
+        System.out.println("Sorted array:     " + inputLines);
+        System.out.println("Chunks: " + chunkify(inputLines));
+        long permutations = 1;
+        for(List<Integer> t : chunkify(inputLines))
+        {
+            permutations *= tribonacci(t.size());
+        }
 
+        System.out.println("Permutations: " + permutations);
     }
 
     private static HashMap<Integer, Integer> getJoltageDifferences(List<Integer> intList)
     {
         HashMap<Integer, Integer> differences = new HashMap();
+
         differences.put(1, 0);
         differences.put(2, 0);
         differences.put(3, 0);
@@ -37,6 +44,47 @@ public class DayTen {
             int diff = intList.get(i+1) - intList.get(i);
             differences.put(diff, differences.get(diff)+1);
         }
+
         return differences;
     }
+
+    private static List<List<Integer>> chunkify(List<Integer> intList) {
+        ArrayList<List<Integer>> chunks = new ArrayList();
+        List<Integer> currentChunk = new ArrayList();
+
+        for (int i = 0; i < intList.size()-1; i++)
+        {
+            int diff = intList.get(i+1) - intList.get(i);
+            currentChunk.add(intList.get(i));
+            if(diff == 3)
+            {
+                chunks.add(List.copyOf(currentChunk));
+                currentChunk = new ArrayList();
+            }
+
+        }
+        int diff = intList.get(intList.size()-1) - intList.get(intList.size()-2);
+        if(diff == 3)
+        {
+            chunks.add(Arrays.asList(intList.get(intList.size()-1)));
+        }
+        else
+        {
+            chunks.get(chunks.size()-1).add(intList.get(intList.size()-1));
+        }
+        return chunks;
+    }
+
+    private static long tribonacci(int l)
+    {
+        if(l <= 1)
+            return 1;
+        if (l == 2)
+            return 1;
+        if(l == 3)
+            return 2;
+        return tribonacci(l -1 ) + tribonacci(l -2) + tribonacci( l - 3);
+    }
+
+
 }
