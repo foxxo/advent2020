ac13dab09f78e61ee65a69b73dc63bbab06e0508
Tod Schlegelmilch <tschlegelmilch@pillartechnology.com>
2020-12-12 11:19:46 -0500

    Day 12 part 2 complete, hacky switch statements and all (no trig for part 2)

--- src/main/java/DayTwelve/Boat.java
+++ src/main/java/DayTwelve/Boat.java
@@ -4,6 +4,7 @@ public class Boat {
     int heading;
     Coord position;
     Coord direction;
+    Coord waypoint;
 
     static final Coord EAST = new Coord(1, 0);
     static final Coord WEST = new Coord(-1, 0);
@@ -15,9 +16,10 @@ public class Boat {
         position = new Coord(posX, posY);
         heading = h;
         direction = new Coord(1, 0);
+        waypoint = new Coord(posX + 10, posY + 1);
     }
 
-    void processOrder(NavOrder o)
+    void processAbsoluteOrder(NavOrder o)
     {
         switch(o.command)
         {
@@ -51,11 +53,62 @@ public class Boat {
         }
     }
 
+    public void processRelativeOrder(NavOrder o)
+    {
+        switch(o.command)
+        {
+            case 'F':
+                position.add(waypoint.times(o.value));
+                break;
+            case 'E':
+                waypoint.add(EAST.times(o.value));
+                break;
+            case 'N':
+                waypoint.add(NORTH.times(o.value));
+                break;
+            case 'W':
+                waypoint.add(WEST.times(o.value));
+                break;
+            case 'S':
+                waypoint.add(SOUTH.times(o.value));
+                break;
+            case 'R':
+                rotateWaypoint(o.value);
+                break;
+            case 'L':
+                rotateWaypoint(360-o.value);
+                break;
+        }
+
+    }
+
+    void rotateWaypoint(int angle)
+    {
+        angle /= 90;
+        Coord newWaypoint = new Coord(0,0);
+        switch(angle) {
+            case 1:
+                newWaypoint.x = waypoint.y;
+                newWaypoint.y  = -waypoint.x;
+                break;
+            case 2:
+                newWaypoint.x = -waypoint.x;
+                newWaypoint.y  = -waypoint.y;
+                break;
+            case 3:
+                newWaypoint.x = -waypoint.y;
+                newWaypoint.y  = waypoint.x;
+                break;
+        }
+        waypoint = newWaypoint;
+    }
+
     void updateDirection()
     {
         double radians = Math.toRadians(heading);
         direction.x = (int)Math.sin(radians);
-        direction.y = (int)Math.cos(radians);;
+        direction.y = (int)Math.cos(radians);
+
     }
 
 }
--- src/main/java/DayTwelve/DayTwelve.java
+++ src/main/java/DayTwelve/DayTwelve.java
@@ -11,9 +11,15 @@ public class DayTwelve {
         List<String> inputLines = AdventUtil.readInputLines("input12");
         Boat myBoat = new Boat(0, 0, 90);
 
-        for(String l : inputLines) {
-            myBoat.processOrder(new NavOrder(l));
-            System.out.println("Boat has moved to " + myBoat.position +  ", heading " + myBoat.heading + " [" + myBoat.direction + "]");
+//        for(String l : inputLines) {
+//            myBoat.processAbsoluteOrder(new NavOrder(l));
+//            System.out.println("Boat has moved to " + myBoat.position +  ", heading " + myBoat.heading + " [" + myBoat.direction + "]");
+//        }
+
+        for(String l : inputLines)
+        {
+            myBoat.processRelativeOrder(new NavOrder(l));
+            System.out.println("Boat has moved to " + myBoat.position +  ", nav waypoint at " + "[" + myBoat.waypoint + "]");
         }
         System.out.println("Manhattan distance from origin: " + (Math.abs(myBoat.position.x) + Math.abs(myBoat.position.y)));
     }
