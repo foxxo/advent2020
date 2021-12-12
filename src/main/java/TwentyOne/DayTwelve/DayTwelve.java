836d1a57e77bcaca63aa45732ceadf342922ef56
Toddo <foxxo.net@gmail.com>
2021-12-12 13:34:37 -0500

    Day 12 part 2 working

--- src/main/java/TwentyOne/DayTwelve/DayTwelve.java
+++ src/main/java/TwentyOne/DayTwelve/DayTwelve.java
@@ -13,58 +13,59 @@ public class DayTwelve {
 
         List<Cave> caves = new ArrayList();
 
-        for(String line: inputLines)
-        {
-           Cave cave1 = new Cave(line.substring(0, line.indexOf("-")));
-           Cave cave2 = new Cave(line.substring(line.indexOf("-")+1));
+        for (String line : inputLines) {
+            Cave cave1 = new Cave(line.substring(0, line.indexOf("-")));
+            Cave cave2 = new Cave(line.substring(line.indexOf("-") + 1));
 
-            if(!caves.contains(cave1)) {
+            if (!caves.contains(cave1)) {
                 caves.add(cave1);
-            }
-           else {
+            } else {
                 cave1 = caves.get(caves.indexOf(cave1));
             }
-            if(!caves.contains(cave2)) {
+            if (!caves.contains(cave2)) {
                 caves.add(cave2);
-            }
-           else {
+            } else {
                 cave2 = caves.get(caves.indexOf(cave2));
             }
             cave1.addPath(cave2);
             cave2.addPath(cave1);
         }
 
-        List<Stack<Cave>> paths = new ArrayList<>();
+        List<Path<Cave>> paths = new ArrayList<>();
         Cave start = caves.get(caves.indexOf(new Cave("start")));
         Cave end = caves.get(caves.indexOf(new Cave("end")));
 
-        Stack<Cave> starter = new Stack<>();
+        Stack<Cave> starter = new Path<>();
         starter.push(start);
 
 
-        getNextPathingSteps(paths, (Stack<Cave>)starter.clone(), start);
+        getNextPathingSteps(paths, (Path<Cave>) starter.clone(), start);
 
 
-        for(Stack<Cave> p : paths)
+        for (Stack<Cave> p : paths)
             System.out.println(p);
         System.out.println(paths.size() + " paths");
 
     }
 
-    static void getNextPathingSteps(List<Stack<Cave>> paths, Stack<Cave> currPath, Cave currCave)
-    {
-        for(Cave c : currCave.connections)
-        {
-            if(!currPath.contains(c) || c.isBig) {
+    static void getNextPathingSteps(List<Path<Cave>> paths, Path<Cave> currPath, Cave currCave) {
+        boolean addedBacktrack = false;
+        for (Cave c : currCave.connections) {
+            if (c.isBig || !currPath.contains(c) || (!currPath.hasBacktrack && !c.name.equals("start"))) {
+                if(currPath.contains(c) && !c.isBig) {
+                    addedBacktrack = true;
+                    currPath.hasBacktrack = true;
+                }
                 currPath.push(c);
 
                 if (c.name.equals("end")) {
                     paths.add(currPath);
                     currPath.pop();
-                }
-                else {
-                    getNextPathingSteps(paths, (Stack<Cave>) currPath.clone(), c);
+                } else {
+                    getNextPathingSteps(paths, (Path<Cave>) currPath.clone(), c);
                     currPath.pop();
+                    if(addedBacktrack)
+                        currPath.hasBacktrack = false;
                 }
 
             }
@@ -73,25 +74,25 @@ public class DayTwelve {
     }
 }
 
-class Cave
-{
+class Path<T> extends Stack<T> {
+    public boolean hasBacktrack;
+}
+
+class Cave {
     String name;
     boolean isBig;
     List<Cave> connections;
-    
-    public Cave(String n)
-    {
+
+    public Cave(String n) {
         name = n;
         isBig = name.equals(name.toUpperCase());
         connections = new ArrayList();
     }
-    
-    public void addPath(Cave c)
-    {
-       if(!connections.contains(c))
-       {
-           connections.add(c);
-       }
+
+    public void addPath(Cave c) {
+        if (!connections.contains(c)) {
+            connections.add(c);
+        }
     }
 
 
@@ -103,8 +104,7 @@ class Cave
         return name.equals(site.name);
     }
 
-    public String toString()
-    {
+    public String toString() {
 //        String conList = connections.stream().map(it -> it.name).reduce(": ", (a,b) -> a + "-" + b);
         return (name);
     }
